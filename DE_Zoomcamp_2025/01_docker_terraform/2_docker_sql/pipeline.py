e1a5eaf02e6d1182b9e435f4722219e9906991b0
maxim-eyengue <maximiliano.thiago.mt@gmail.com>
2025-01-25 20:54:26 +0100

    updated container dockerfile and data pipeline

--- DE_Zoomcamp_2025/01_docker_terraform/2_docker_sql/pipeline.py
+++ DE_Zoomcamp_2025/01_docker_terraform/2_docker_sql/pipeline.py
@@ -1,5 +1,11 @@
 # Necessary import
+import sys
 import pandas as pd
 
-# Check pandas
-print(f"job finished with success: {pd.__version__}")
+# print command line arguments
+print(sys.argv)
+# Get the first command line argument as day
+day = sys.argv[1]
+
+# Check pandas and pipeline arguments
+print(f"job finished with success the {day}: pandas version {pd.__version__}.")
